808873bfd4266350c2d31e2b45113394cf54210e
TravisDickens <Travis.Dickens@outlook.com>
2025-07-19 20:09:19 +0200

    Created Repository Interfaces

---
+++ blog/src/main/java/com/travis/blog/repositories/PostRepository.java
@@ -0,0 +1,13 @@
+package com.travis.blog.repositories;
+
+import com.travis.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID>
+{
+
+}
---
+++ blog/src/main/java/com/travis/blog/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.travis.blog.repositories;
+
+import com.travis.blog.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>
+{
+
+}
